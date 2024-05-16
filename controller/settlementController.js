0e1fe77bae2391735cea2234e5da683c3a932d3b
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-05-16 17:40:54 +0530

    ADD : Added the settlement controller

---
+++ controller/settlementController.js
@@ -0,0 +1,48 @@
+// controllers/settlementDetailsController.js
+import settlementDetailsService from '../service/settlementService.js';
+import fs from 'fs';
+
+const createSettlementDetails = async (req, res) => {
+    const { settlementType, accountNo, bankName, branchName } = req.body;
+    try {
+        const newSettlementDetails = await settlementDetailsService.createSettlementDetails(settlementType, accountNo, bankName, branchName);
+        res.json(newSettlementDetails);
+    } catch (err) {
+        console.error('Error creating settlement details', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const getAllSettlementDetails = async (req, res) => {
+    try {
+        const { limit, offset, bankName, branchName } = req.query;
+        const settlementDetails = await settlementDetailsService.getAllSettlementDetails(limit, offset, bankName, branchName);
+        res.json(settlementDetails);
+    } catch (err) {
+        console.error('Error getting settlement details', err);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+const exportToExcel = async (req, res) => {
+    const filePath = 'settlementDetails.xlsx';
+    try {
+        await settlementDetailsService.exportToExcel(filePath);
+        res.download(filePath, (err) => {
+            if (err) {
+                throw new Error('Error downloading file');
+            } else {
+                // Delete the file after download
+                fs.unlinkSync(filePath);
+            }
+        });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+export default {
+    createSettlementDetails,
+    getAllSettlementDetails,
+    exportToExcel
+};
