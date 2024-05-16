4d78277134787a11c88ab1fdb0c267c9249b314b
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-05-16 17:40:44 +0530

    ADD : Added the seller controller

---
+++ controller/sellerController.js
@@ -0,0 +1,48 @@
+// controllers/sellerController.js
+import sellerService from '../service/sellerService.js';
+import fs from 'fs';
+
+const createSeller = async (req, res) => {
+  const { gst, pan, bppId, name } = req.body;
+  try {
+    const newSeller = await sellerService.createSeller(gst, pan, bppId, name);
+    res.json(newSeller);
+  } catch (err) {
+    console.error('Error creating seller', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const getAllSellers = async (req, res) => {
+  try {
+    const { limit, offset, name, gst, pan } = req.query;
+    const sellers = await sellerService.getAllSellers(limit, offset, name, gst, pan);
+    res.json(sellers);
+  } catch (err) {
+    console.error('Error getting sellers', err);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
+
+const exportToExcel = async (req, res) => {
+  const filePath = 'sellers.xlsx';
+  try {
+    await sellerService.exportToExcel(filePath);
+    res.download(filePath, (err) => {
+      if (err) {
+        throw new Error('Error downloading file');
+      } else {
+        // Delete the file after download
+        fs.unlinkSync(filePath);
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+export default {
+  createSeller,
+  getAllSellers,
+  exportToExcel
+};
