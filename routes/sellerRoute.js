bd9229fbf9936381d89c1837d27e82a2fba228c1
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-05-21 19:19:39 +0530

    CHG : Modified the seller route to move the salesReport route to top

--- routes/sellerRoute.js
+++ routes/sellerRoute.js
@@ -1,10 +1,10 @@
 // routes/sellerRoutes.js
-
 import express from 'express';
 import sellerController from '../controller/sellerController.js';
 
 const router = express.Router();
 
+router.get('/sellers/salesReport', sellerController.getSalesReport);
 router.post('/sellers', sellerController.createSeller);
 router.get('/sellers', sellerController.getAllSellers);
 router.get('/sellers/:id', sellerController.getSellerById);
