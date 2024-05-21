14fabd44663e73724d710aea90236a7910879f7e
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-05-21 19:21:12 +0530

    ADD : Added user model

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+import { DataTypes } from 'sequelize';
+
+const User = (sequelize) => {
+    const UserModel = sequelize.define('User', {
+        id: {
+            type: DataTypes.UUID,
+            primaryKey: true,
+            defaultValue: DataTypes.UUIDV1,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            defaultValue: sequelize.literal('extract(epoch from now()) * 1000'),
+        },
+        updatedAt: {
+            type: DataTypes.BIGINT,
+            allowNull: false,
+            defaultValue: sequelize.literal('extract(epoch from now()) * 1000'),
+        },
+    }, {
+        freezeTableName: true,
+        timestamps: true,
+        createdAt: 'createdAt',
+        updatedAt: 'updatedAt',
+        underscored: true,
+    });
+
+    return UserModel;
+};
+
+export default User;
