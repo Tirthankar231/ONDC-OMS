a3249072bc7a3306914407166d0fc20dcd0a7053
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-05-21 13:16:28 +0530

    ADD : Added the getOne routes and state count routes

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -7,6 +7,8 @@ const router = express.Router();
 
 router.post('/orders', orderController.createOrder);
 router.get('/orders', orderController.getAllOrders);
+router.get('/orders/:id', orderController.getOrderById);
+router.get('/orders/state/count', orderController.getOrderStateCountsController);
 router.get('/orders/exportExcel', orderController.exportToExcel);
 
 export default router;
